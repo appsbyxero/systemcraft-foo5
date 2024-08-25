0b377a77ff2edb3f47730dcfde204be119d96895
appsbyxero <appxero@gmail.com>
2024-08-25 12:38:48 +0530

    Provide starter code

--- app/main.ts
+++ app/main.ts
@@ -1,3 +1,11 @@
 import * as process from 'process';
+import * as fs from 'fs';
+import * as rd from 'readline'
 
-// Write your code here!
+// This passes the first task!
+
+var reader = rd.createInterface(fs.createReadStream(process.argv[3]));
+
+reader.on("line", (l: string) => {
+    console.log(l);
+})
